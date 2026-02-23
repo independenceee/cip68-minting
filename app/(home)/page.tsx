b0d188cf9d0ecd5514e93dc5ce0c65797bbf2baa
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2026-02-23 14:44:20 +0700

    feat: update homepage for deploy vercel

---
+++ app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="min-h-screen bg-gray-900 text-white">
+            <h1 className="text-3xl font-bold">Welcome to the CIP68 Course</h1>
+        </div>
+    )
+}
