8f08041a6a4dd2b3bba9f62268892c888cb63fb2
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2026-03-24 22:21:11 +0700

    feat: Before continuing, make sure you have configured: We will use Fumadocs MDX as a content source, you can configure it first

--- app/document/layout.tsx
+++ app/document/layout.tsx
@@ -1,11 +1,14 @@
 import { source } from "@/libs/source";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { baseOptions } from "@/libs/layout.shared";
+import { RootProvider } from "fumadocs-ui/provider/next";
 
 export default function Layout({ children }: LayoutProps<"/document">) {
     return (
-        <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
-            {children}
-        </DocsLayout>
+        <RootProvider>
+            <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+                {children}
+            </DocsLayout>
+        </RootProvider>
     );
 }
