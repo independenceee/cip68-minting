a0d24100bdcefc89da147a2a35d5e2c288565aa2
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2026-06-16 14:17:43 +0700

    feat(cip68): implement CIP-68 minting policy and asset generation

--- frontend/src/helpers/src/providers/cardano/blockfrost.ts
+++
@@ -1,6 +0,0 @@
-import { BlockfrostProvider } from "@meshsdk/core";
-import { BLOCKFROST_API_KEY } from "../../constants/enviroments.constant";
-
-const blockfrostProvider = new BlockfrostProvider(BLOCKFROST_API_KEY);
-
-export default blockfrostProvider;
